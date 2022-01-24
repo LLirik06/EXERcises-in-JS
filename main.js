0b5d99cde79c3f18844445d47096347cc4dd4627
Kirill <dremkiri@gmail.com>
2022-01-24 14:05:24 +0200

    4

---
+++ main.js
@@ -0,0 +1,210 @@
+// Beginer 1
+// let a = +prompt('Введите сторону квадрата')
+// let b = 4 * a
+// alert(b)
+// console.log(b)
+
+
+// Beginer 2
+// let a = +prompt('Введите сторону квадрата чтоб найти его площадь')
+// let b = a ** 2
+// alert(b)
+// console.log(b)
+
+
+// Beginer 3
+// let a = +prompt('Введите первую сторону')
+// let b = +prompt('Введите вторую сторону')
+// let S = a * b
+// let P = 2*(a + b)
+// alert('Площадь равна ' + S)
+// alert('Периметр равен ' + P)
+// console.log(S)
+// console.log(P)
+
+
+// Beginer 4 
+// let d = +prompt('Введите диаметр круга')
+// let L = d * 3.14
+// alert(L)
+// console.log(L)
+
+// Beginer 5 
+// let a = +prompt('Введите длину ребра')
+// let V = a ** 3
+// let S = 6 * (a ** 2)
+// alert('Объем равен ' + V)
+// alert('Площадь равна '+ S)
+// console.log(V)
+// console.log(S)
+
+
+// Beginer 6
+// let a = +prompt('Введите длину')
+// let b = +prompt('Введите ширину')
+// let c = +prompt('Введите высоту')
+// let V = a * b * c
+// let S = 2 * ((a * b) + (b * c) + (a * c))
+// alert('Объем равен ' + V)
+// alert('Площадь равна '+ S)
+// console.log(V)
+// console.log(S)
+
+
+// Beginer 7 
+// let R = +prompt('Введите радиус')
+// let S = 3.14 * (R ** 2) 
+// let L = 2 * 3.14 * R
+// alert('Площадь равна '+ S)
+// alert('Длина круга равна '+ L)
+// console.log(L)
+// console.log(S)
+
+
+// Beginer 8
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// let sr = (a + b) / 2
+// alert(sr)
+// console.log(sr)
+
+
+// Beginer 9
+// let a = +prompt('Введите первое число (положительное)')
+// let b = +prompt('Введите второе число (положительное)')
+// let cor = (a * b) ** 0.5
+// alert(cor)
+// console.log(cor)
+
+
+// Begin 10 
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// a != 0
+// b != 0
+// let Sum = (a ** 2) + (b ** 2)
+// let Razn = (a ** 2) - (b ** 2)
+// let Proizv = (a ** 2) * (b ** 2)
+// let Chast = (a ** 2) /  (b ** 2)
+// alert('Сумма квадратов равна ' + Sum)
+// alert('Разность  квадратов равна ' + Razn)
+// alert('Произведение квадратов равна ' + Proizv)
+// alert('Частное квадратов равна ' + Chast)  
+// console.log(Sum)
+// console.log(Razn)
+// console.log (Proizv)
+// console.log (Chast)
+
+
+//Begin 11
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// a != 0
+// b != 0
+// a > 0
+// b > 0
+// let Sum = a + b
+// let Razn = a - b
+// let Proizv = a * b
+// let Chast = a /  b
+// alert('Сумма модулей равна ' + Sum)
+// alert('Разность модулей равна ' + Razn)
+// alert('Произведение модулей равна ' + Proizv)
+// alert('Частное модулей равна ' + Chast) 
+// console.log(Sum)
+// console.log(Razn)
+// console.log (Proizv)
+// console.log (Chast)
+
+
+// Beginer 12
+// let a = +prompt('Введите длину первого катета')
+// let b = +prompt('Введите длину второго катета')
+// let c = ((a**2)+(b**2))**0.5
+// let P = a + b + c
+// alert('Длина гипотенузы равна ' + c)
+// alert('Периметр равен' + P)
+// console.log(c)
+// console.log(P)
+
+
+// Beginer 13
+// let R = +prompt('Введите радиус большего круга')
+// let r = +prompt ('Введите радиус меньшего круга')
+// let S = 3.14 * (R ** 2)
+// let s = 3.14 * (r ** 2)
+// let s3 = S - s
+// alert('Площадь большего равна ' + S)
+// alert('Площадь меньшего равна' + s)
+// alert('Площадь кольца равна ' + s3)
+// console.log(S)
+// console.log(s)
+// console.log(s3)
+
+
+// Beginer 14
+// let L = +prompt('Введите длину окружности')
+// let R = L / (2 * 3.14) 
+// let S = 3.14 * (R ** 2)
+// alert('Радиус равен ' + R)
+// alert('Площадь круга равна+ S')
+// console.log(R)
+// console.log(S)
+
+
+// Beginer 15
+// let S = +prompt('Введите площадь круга')
+// let D = ((4 * S) / 3.14) ** 0.5
+// let L = 3.14 * D
+// alert('Диаметр равен ' + D)
+// alert('Длина окружности равна ' + L)
+// console.log(D)
+// console.log(L)
+
+
+// Beginer 25
+// let x = +prompt('Введите х')
+// let y = 3*(x**6) - 6*(x**2) - 7
+// alert(y)
+// console.log(y)
+
+
+// Beginer 26
+// let x = +prompt('Введите х')
+// let y = (4 * ((x - 3) ** 6)) - (7 * ((x - 3) ** 3) ) +  2 
+// alert(y)
+// console.log(y)
+
+
+//Beginer 27
+// let A = +prompt('Введите число А')
+// let A2 = A ** 2
+// let A4 = A2 * A2
+// let A8 = A4 * A4
+// alert('А в степени 2 равно ' + A2)
+// alert('А в степени 4 равно ' + A4)
+// alert('А в степени 8 равно ' + A8)
+// console.log(A2)
+// console.log(A4)
+// console.log(A8)
+
+
+// Beginer 28
+// let A = +prompt('Введите число А')
+// let A2 = A * A 
+// let A3 = A2 * A
+// let A5 = A2 * A3
+// let A10 = A5 * A5
+// let A15 = A10 * A5
+// alert('А в степени 2 равно ' + A2)
+// alert('А в степени 3 равно ' + A3)
+// alert('А в степени 5 равно ' + A5)
+// alert('А в степени 10 равно ' + A10)
+// alert('А в степени 15 равно ' + A15)
+// console.log(A2)
+// console.log(A3)
+// console.log(A5)
+// console.log(A10)
+// console.log(A15)
+
+
